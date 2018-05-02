3b5a3cd1f28f2c1a8d6a7122fd8dfef5bccdb1d0
leny <leny@172.17.128.157>
2018-05-02 19:18:48 +0900

    Add_Prob3

--- src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
@@ -1,5 +1,21 @@
 package edu.handong.csee.java.lab13.prob3;
 
-public class Circle {
-
+public class Circle extends Shape{
+	private double radious;
+	
+	public Circle(double r) {
+		radious = r;
+	}
+	
+	public double area() {
+		return Math.PI * Math.pow(radious, 2);
+	}
+	
+	public double perimeter() {
+		return 2.0 * Math.PI * radious;
+	}
+	
+	public double get_radious() {
+		return radious;
+	}
 }
