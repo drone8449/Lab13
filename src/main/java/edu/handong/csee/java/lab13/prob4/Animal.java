42b389cd6a35b43072326ef4908c984f55ea2413
leny <leny@172.17.128.157>
2018-05-02 20:03:24 +0900

    Add_Prob4

--- src/main/java/edu/handong/csee/java/lab13/prob4/Animal.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Animal.java
@@ -1,5 +1,12 @@
 package edu.handong.csee.java.lab13.prob4;
 
 public class Animal {
-
+	private String name;
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void getName(){
+		System.out.println("Name: " + name);
+	}
 }
--- src/main/java/edu/handong/csee/java/lab13/prob4/Pet.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Pet.java
@@ -1,5 +1,5 @@
 package edu.handong.csee.java.lab13.prob4;
 
-public class Pet {
-
+public interface Pet {
+	public String food();
 }
